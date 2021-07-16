ffcccaf6cdad0b7e7740caba7981f6709fa37f95
Kittipong chaimala <chaimala335@gmail.com>
2021-07-17 04:28:45 +0700

    'dsf'

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'qQ1G9SGgcSA6VLbCsV87Qe8OghrUcT/6Dtvz6c20yeMD10g40EKF5kXKI/8yQQKQmPhIedKeeiE8COt31a8HSU7GDdKlQmIXV55DseMSzok5UMMD45QiNIJkcSRp6wK+aPnq0KLPTNnguI49hRaN/AdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
